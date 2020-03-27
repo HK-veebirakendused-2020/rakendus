b1b14b1bc65173a20979718bb3eaf00823fea1f4
Andrus Rinde <andrusrinde@users.noreply.github.com>
2020-03-27 15:19:23 +0200

    3tund

--- tund3/fnc_users.php
+++ tund3/fnc_users.php
@@ -2,7 +2,7 @@
 function signUp($name, $surname, $email, $gender, $birthDate, $password){
 	$notice = null;
 	$conn = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUserName"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
-	$
+	$stmt = $conn->prepare("");
 	
 	$stmt->close();
 	$conn->close();
